cfe752ee56dca54c7f0e9f0903bb7662430e9add
Thomrl <tommer.rl@live.dk>
2020-06-13 18:25:06 +0200

    Added option to set time + Rewritten js (clean up)

--- js/index-apps.js
+++ js/index-apps.js
@@ -1,4 +1,6 @@
 var button = document.getElementById("WUW-button");
+var button2 = document.getElementById("WUW2-button");
+var domInput = document.getElementById("timeinput");
 var domWUW1 = document.getElementById("wake-up-time-1");
 var domWUW2 = document.getElementById("wake-up-time-2");
 var domWUW3 = document.getElementById("wake-up-time-3");
@@ -11,35 +13,53 @@ var domWUW8 = document.getElementById("wake-up-time-8");
 
 button.addEventListener('click', wakeUpWhen);
 
+button2.addEventListener('click', function() {
+    var userInput = domInput.value;
+    userSetTime = userInput.split(':')
+    userSetHour = userSetTime[0]
+    userSetMinute = userSetTime[1]
+    ssTime = moment().set({'hour': userSetHour, 'minute': userSetMinute});
+    console.log(userSetTime);
+    
+    wakeUpWhenPluss(45, domWUW1);
+    wakeUpWhenPluss((15+90), domWUW2);
+    wakeUpWhenPluss((15+(2*90)), domWUW3);
+    wakeUpWhenPluss((15+(3*90)), domWUW4);
+    wakeUpWhenPluss((15+(4*90)), domWUW5);
+    wakeUpWhenPluss((15+(5*90)), domWUW6);
+    wakeUpWhenPluss((15+(6*90)), domWUW7);
+    wakeUpWhenPluss((15+(7*90)), domWUW8);
+    document.getElementById("wake-up-when").style.display = "block";
+})
+
 
 function wakeUpWhen() {
     //document.getElementById("cur-hours").textContent = moment().format("HH:mm");
-
     document.getElementById("wake-up-when").style.display = "block";
 
-    var ccTime = moment().add(45, 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW1.textContent = ccTime.format("HH:mm");
-    var ccTime = moment().add((15+(2*90)), 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW2.textContent = ccTime.format("HH:mm");
-    var ccTime = moment().add((15+(3*90)), 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW3.textContent = ccTime.format("HH:mm");
-    var ccTime = moment().add((15+(4*90)), 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW4.textContent = ccTime.format("HH:mm");
-    var ccTime = moment().add((15+(5*90)), 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW5.textContent = ccTime.format("HH:mm");
-    var ccTime = moment().add((15+(6*90)), 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW6.textContent = ccTime.format("HH:mm");
-    var ccTime = moment().add((15+(7*90)), 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW7.textContent = ccTime.format("HH:mm");
-    var ccTime = moment().add((15+(8*90)), 'minutes');
-    console.log(ccTime.format('hh:mm'));
-    domWUW8.textContent = ccTime.format("HH:mm");
+    wakeUpWhenPlus(45, domWUW1);
+    wakeUpWhenPlus((15+90), domWUW2);
+    wakeUpWhenPlus((15+(2*90)), domWUW3);
+    wakeUpWhenPlus((15+(3*90)), domWUW4);
+    wakeUpWhenPlus((15+(4*90)), domWUW5);
+    wakeUpWhenPlus((15+(5*90)), domWUW6);
+    wakeUpWhenPlus((15+(6*90)), domWUW7);
+    wakeUpWhenPlus((15+(7*90)), domWUW8);
 }
 
+function wakeUpWhenPlus(addedMins, whichDOM) {
+    var ccTime = moment().add(addedMins, 'minutes');
+    whichDOM.textContent = ccTime.format("HH:mm");
+}
+
+function wakeUpWhenPluss(addedMins, whichDOM) {
+    ssTime = moment().set({'hour': userSetHour, 'minute': userSetMinute});
+    var ccTime = ssTime.add(addedMins, 'minutes');
+    whichDOM.textContent = ccTime.format("HH:mm");
+}
+
+
+function wakeUpWhenMinus(subtdMins, whichDOM) {
+    var ccTime = moment().subtract(subtdMins, 'minutes');
+    whichDOM.textContent = ccTime.format("HH:mm");
+}
